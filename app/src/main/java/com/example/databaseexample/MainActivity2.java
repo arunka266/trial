e0b275e989b1b2b502b98ba1a052a63a77d8a990
Arunka266 <arunka266@gmail.com>
2021-03-26 14:25:41 +0530

    database

---
+++ app/src/main/java/com/example/databaseexample/MainActivity2.java
@@ -0,0 +1,25 @@
+package com.example.databaseexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        findViewById(R.id.textViewLogin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //if user pressed on login
+                //we will open the login screen
+                startActivity(new Intent(MainActivity2.this, MainActivity.class));
+            }
+        });
+    }
+}
